ecb95c15d289a5d9254ca22ae2256ee3729c5e6e
SND\Technovert_cp <SND\Technovert_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2013-09-23 09:28:14 +0000

    Changed source control to codeplex. Reduce template geneation path.
    git-svn-id: https://angularstart.svn.codeplex.com/svn@85358 9b283d60-5439-405e-af05-b73fd8c4d996

--- AngularStart/build.cmd
+++ AngularStart/build.cmd
@@ -2,9 +2,9 @@
 cd %~dp0
 cd AngularStart
 REM create the project template zip
-..\tools\7za.exe a -tzip -mx9 ..\ProjectTemplates\CSharp\Web\AngularStart.zip *
+..\tools\7za.exe a -tzip -mx9 ..\t\AngularStart.zip *
 pushd %~dp0
 REM create the VSIX file based on the zip and manifest data
 tools\7za.exe a -tzip -mx9 AngularStart.vsix @vsixfiles.txt
 REM delete the temporary zip
-rmdir /s /q ProjectTemplates
+rmdir /s /q t
